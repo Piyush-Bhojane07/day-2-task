f77eb9332ab020c8e5fd8e1492429628b39709a6
Piyush-Bhojane <90183830+Piyush-Bhojane07@users.noreply.github.com>
2023-01-10 15:33:17 +0530

    Initial commit

---
+++ Table.sql
@@ -0,0 +1,10 @@
+CREATE table Person_Info(
+PersonId int,
+First_Name VARCHAR2(250),
+Last_Name VARCHAR2(250),
+Location VARCHAR2(100),
+Address VARCHAR2(350),
+Education CHAR(50),
+Contact_No CHAR(10)
+);
+
---
+++ insert.sql
@@ -0,0 +1,49 @@
+insert into Person_Info(first_name, last_name, location, address, education, contact_no)
+values ('F. 1', 'L. 1', 'l1', 'a1', 'e1', 'c. n1');
+
+select * from person_info;
+
+insert into person_info(personid, first_name, last_name, location, address, education, contact_no) 
+values ('22', 'f2', 'L. 2', 'Loc 2', 'add 2', 'edu 2', 'con. no 2');
+
+select * from person_info;
+
+insert into person_info(personid, first_name, last_name, location, address, education, contact_no)
+values ('33', 'f3', 'l3', 'Loc 3', 'add 3', 'edu 3', 'con. no 3');
+select * from person_info;
+
+INSERT into person_info(personid, first_name, last_name, location, address, education, contact_no)
+values('44', 'f4', 'l4', 'loc 4', 'add 4', 'edu 4', '4444');
+
+select * from person_info;
+
+insert into person_info(personid, first_name, last_name, location, address, education, contact_no)
+values('55', 'f5', 'l5', 'loc 5', 'add 5', 'edu 5', '555555');
+
+insert into person_info(personid, first_name, last_name, location, address, education, contact_no)
+values('66', 'f6', 'l6', 'loc 6', 'add 6', 'edu 6', '6666');
+
+insert into person_info(personid, first_name, last_name, location, address, education, contact_no)
+values ('77', 'f7', 'l7', 'loc 7', 'Add 6', 'edu 6', '7777');
+
+insert into person_info(personid, first_name, last_name, location, address, education, contact_no)
+values('88', 'f8', 'l8', 'loc 8', 'Add 8', 'edu 8', 'con 8');
+
+insert into person_info(personid, first_name, last_name, location, address, education, contact_no)
+values ('99' , 'f9', 'l 9', 'loc 9' , ' add 9', 'edu 9', '9999');
+
+insert into person_info(personid, first_name, last_name, location, address, education, contact_no)
+values('10', 'f10', 'l 10', 'loc 10', 'add 10', 'edu 10', 'con 10');
+
+select first_name, last_name from person_info;
+
+select * from person_info;
+
+insert into person_info values(11, 'f11', 'l 11', 'loc 11', 'add 11', 'edu 11', 'con 11');
+
+select * from person_info;
+
+insert into person_info values (12, 'f 12', 'L 12', 'loc 12', 'add 12', 'edu 12', '121212');
+
+
+select * from person_info;
